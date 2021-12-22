0827eb71599ffecf16507dafa1349e7f4ca82762
MousamSingh2612 <singhmausam76@gmail.com>
2021-12-23 02:07:46 +0530

    From AS

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+import 'story_brain.dart';
+class Story
+{
+  String storyTitle = 'a';
+  String choice1 = 'a';
+  String choice2 = 's';
+
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
+}
+
